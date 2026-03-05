113f05de3c0f9cd12e3bfdde24a025eec1527f58
Vinnu0896 <vinodkumarkatakam8@gmail.com>
2026-03-06 00:24:39 +0530

    chore: terraform fmt

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-backend "s3" {
-bucket         = "gitops-project-tf-state-bucket-us-east-1"
-key            = "eks/gitops-project/terraform.tfstate"
-region         = "us-east-1"
-dynamodb_table = "gitops-project-terraform-lock-table-us-east-1"
-encrypt        = true
-}
+  backend "s3" {
+    bucket         = "gitops-project-tf-state-bucket-us-east-1"
+    key            = "eks/gitops-project/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "gitops-project-terraform-lock-table-us-east-1"
+    encrypt        = true
+  }
 }
